c7a8265593af0cbb8d3ad739af817d97b04f2017
Tamil selvan <Tamilps2@users.noreply.github.com>
2013-08-26 21:53:38 +0530

    Update SessionDBInterface.php

--- SessionDBInterface.php
+++ SessionDBInterface.php
@@ -1,41 +1,40 @@
 <?php
 /**
- * Defines the interface that must be implemented for a db session handling
- * class
+ * Defines the interface that must be implemented by the 
+ * Database class that implements the session handling.
  */
 interface SessionDBInterface {
      
   /**
-    * Read the data corresponding to the $sessionId from the session database
-    * 
-    * @param type $sessionId Id of the session to which data must be read
-    * @return string|boolean
-    */
-   public function readSessionData($sessionId);
+   * Read the data corresponding to the $sessionId from the session database
+   * 
+   * @param type $sessionId Id of the session to which data must be read
+   * @return string|boolean
+   */
+  public function readSessionData($sessionId);
 
-   /**
-    * Writes the given session data to the session database.
-    * 
-    * @param type $sessionId
-    * @param type $sessionData
-    * @return boolean
-    */
-   public function writeSessionData($sessionId, $sessionData);
+  /**
+   * Writes the given session data to the session database.
+   * 
+   * @param type $sessionId
+   * @param type $sessionData
+   */
+  public function writeSessionData($sessionId, $sessionData);
 
-   /**
-    * Deletes the session data for the given sessionId from the database.
-    * 
-    * @param type $sessionId
-    * @return boolean
-    */
-   public function deleteSessionData($sessionId);
+  /**
+   * Deletes the session data for the given sessionId from the database.
+   * 
+   * @param type $sessionId
+   * @return boolean
+   */
+  public function deleteSessionData($sessionId);
 
-   /**
-    * Check if a session for the current $sessionId exists.
-    * 
-    * @param type $sessionId
-    * @return boolean
-    */
-   public function checkSession($sessionId);
+  /**
+   * Check if a session for the current $sessionId exists.
+   * 
+   * @param type $sessionId
+   * @return boolean
+   */
+  public function checkSession($sessionId);
 }
 ?>
