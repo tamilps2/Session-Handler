5c3945aac657ba1e5d1d947b372b14c69d8e5524
Tamil selvan <Tamilps2@users.noreply.github.com>
2013-08-28 08:37:42 +0530

    fixed bug

--- __test__/SessionHandlerTestCase.php
+++ __test__/SessionHandlerTestCase.php
@@ -1,9 +1,9 @@
 <?php
-include (__dir__ . '/../HB_SessionHandler.php');
+include (__dir__ . '/../SessionHandler.php');
 
-$mysqli = new mysqli('localhost', 'root', 'root', 'hbsession');
+$mysqli = new mysqli('localhost', 'root', 'root', 'session');
 $sessiondb = new MysqlSessionDB($mysqli);
-$session = new HB_SessionHandler($sessiondb);
+$session = new SessionHandler($sessiondb);
 
 session_set_save_handler($session, true);
 session_start();
